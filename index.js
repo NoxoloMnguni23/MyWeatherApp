bbc6172bd0732b9778fa13657b6d96625d2ab874
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2023-12-29 09:30:27 +0200

    updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function getWeather() {
     const apiKey = 'e4feae1faa2d5a8f8b3f4f0d4d45469e';
-    const city = document.getElementById('location').ariaValueMax;
+    const city = document.getElementById('location').value;
 
 
-    if (!location) {
+    if (!city) {
         alert('Please enter a location')
         return;
     }
@@ -15,28 +15,27 @@ function getWeather() {
     fetch(currentWeatherUrl)
         .then(Response => Response.json())
         .then(data => {
+            console.log(data)
             displayWeather(data);
 
         })
         .catch(error => {
-            console.error('Error fetching current weather data', error);
-            alert('Error fetching current weather data. Please try again.');
+            console.log('Error fetching current weather data', error);
 
         });
 
-    fetch( forecastUrl)
+    fetch(forecastUrl)
         .then(Response => Response.json())
         .then(data => {
             displayforecast(data.list);
 
         })
         .catch(error => {
-            console.error('Error fetching weather forecast data', error);
-            alert('Error fetching  weather forecast data. Please try again.');
+            console.log('Error fetching weather forecast data', error);
 
         });
 }
-function displayWeather(data){
+function displayWeather(data) {
     const tempatureInfo = document.getElementById('tempature');
     const weather = document.getElementById('weather-details');
     const hourlyforecast = document.getElementById('hourly');
@@ -45,17 +44,25 @@ function displayWeather(data){
     weather.innerHTML = '';
     hourlyforecast.innerHTML = '';
     tempatureInfo.innerHTML = '';
-     
-    if(data.cod === '404'){
+
+    if (data.cod === '404') {
         weather.innerHTML = `<p>${data.message}</p>`;
     }
-    else{
+    else {
         const cityName = data.name;
         const tempature = Math.round(data.main.temp - 273.15);
         const description = data.weather[0].description;
         const iconCode = data.weather[0].icon;
-        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+        const iconUrl = `http://openweathermap.org/img/w/${iconCode}`;
 
-        const tempatureHTML = `<p>${tempature}`
+        const tempatureHTML = `<p>${tempature}°C</p>`;
+        const weatherHTML = `<p>${cityName}</p><p>${description}</p>`;
+
+        tempatureInfo.innerHTML = tempatureHTML;
+        weather.innerHTML = weatherHTML;
+        weatherIcon.src = iconUrl;
+        weatherIcon.alert = description;
+
+        showImage();
     }
 }
