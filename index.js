ab5249b80c127e2426058dd355912db2ca8640d3
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-01-06 02:06:45 +0200

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function getWeather() {
         })
         .catch(error => {
             console.log('Error fetching current weather data', error);
-    
+
 
         });
 
@@ -34,6 +34,7 @@ function getWeather() {
         weather.innerHTML = '';
         tempatureInfo.innerHTML = '';
 
+
         if (data.cod === '404') {
             weather.innerHTML = `<p>${data.message}</p>`;
         }
@@ -43,6 +44,9 @@ function getWeather() {
             const description = data.weather[0].description;
             const iconCode = data.weather[0].icon;
             const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+            //const timezone1 = data.timezone.id;
+
+
 
             const tempatureHTML = `<p>${tempature}°C</p>`;
             const weatherHTML = `<p>${cityName}</p><p>${description}</p>`;
@@ -52,11 +56,41 @@ function getWeather() {
             weatherIcon.src = iconUrl;
             weatherIcon.alert = description;
 
+            getDate();
             showImage();
+            hourlyforecast(data);
         }
     }
     function showImage() {
         const weatherIcon = document.getElementById('weather-icon');
         weatherIcon.style.display = 'block';
     }
-}
+
+
+    function getDate() {
+
+        document.getElementById('date').innerHTML = new Date();
+        //const currentDateTime = new Intl.DateTimeFormat("en-US", {timeZone: `${timezone1}`, timeStyle: 'short', hour12: true}).format(date);
+        //document.getElementById('time').innerHTML = currentDateTime;
+        //document.getElementById('date').innerHTML = new Date().toDateString("en-US", { timeZone: `${timezone1}`, timeStyle: 'medium', hourCyle: 'h24' });
+    }
+
+    function hourlyforecast(data) {
+
+        let now = new Date().getHours();
+        let time1 = now + 1;
+        let time2 = time1 + 1;
+        //let time3 = time2 + 1;
+
+
+        const hour1 = data.hourly[1].temp;
+        const hour2 = data.hourly[2].temp;
+        const hour3 = data.hourly[3].temp;
+
+
+        document.getElementById("hour-one").innerHTML = hour1 + "°C";
+        document.getElementById("hour-two").innerHTML = hour2 + "°C";
+        document.getElementById("hour-three").innerHTML = hour3 + "°C";
+
+
+    }}
